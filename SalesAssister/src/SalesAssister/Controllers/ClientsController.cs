7082983f3c4c60f2eef82f7fd65aff88bf4ccb19
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-22 13:15:53 -0700

    can list all clients for a sales person trying to deal with sales people with no clients

---
+++ SalesAssister/src/SalesAssister/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using SalesAssister.Models;
+using Microsoft.Data.Entity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalesAssister.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private SalesAssisterDbContext db = new SalesAssisterDbContext();
+
+        public IActionResult Index(int id)
+        {
+            var clients = db.Clients.Where(x => x.SalesPersonId == id).Include(x => x.salesperson).ToList();
+
+            if(clients.Count >= 1)
+            {
+                return View(clients);
+            }
+            else
+            {
+                return View(db.SalesPersons.ToList());
+            } 
+        }
+    }
+}
