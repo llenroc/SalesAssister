3f55b708a73934f9331054081ce5fba74c16b860
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-22 10:11:11 -0700

    start the data models

---
+++ SalesAssister/src/SalesAssister/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SalesAssister.Models
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int SalesPersonId { get; set; }
+        public virtual SalesPerson salesperson { get; set; }
+    }
+}
---
+++ SalesAssister/src/SalesAssister/Models/SalesPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesAssister.Models
+{
+    [Table("SalesPersons")]
+    public class SalesPerson
+    {
+        [Key]
+        public int SalesPersonId { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }  
+    }
+}
