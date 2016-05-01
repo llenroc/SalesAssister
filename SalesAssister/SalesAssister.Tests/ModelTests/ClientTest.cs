ccc89987f609ec541cd49b2ebe00a53c84f99404
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-30 17:29:21 -0700

    add test solution, all 8 test pass for model data.

---
+++ SalesAssister/SalesAssister.Tests/ModelTests/ClientTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesAssister.Models;
+using Xunit;
+
+namespace SalesAssister.Tests
+{
+    public class ClientTest
+    {
+        [Fact]
+        public void GetClientNameTest()
+        {
+            //arrange
+            var client = new Client();
+            client.Name = "Bob Jones";
+
+            //act
+            var result = client.Name;
+
+            //assert
+            Assert.Equal("Bob Jones", result);
+        }
+
+        [Fact]
+        public void GetClientEmailTest()
+        {
+            //arrange
+            var client = new Client();
+            client.Email = "Bob@email.com";
+
+            //act
+            var result = client.Email;
+
+            //assert
+            Assert.Equal("Bob@email.com", result);
+        }
+
+        [Fact]
+        public void GetClientIdTest()
+        {
+            //arrange
+            var client = new Client();
+            client.ClientId = 1;
+
+            //act
+            var result = client.ClientId;
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void GetClientPhoneTest()
+        {
+            //arrange
+            var client = new Client();
+            client.Phone = "503-635-6779";
+
+            //act
+            var result = client.Phone;
+
+            //assert
+            Assert.Equal("503-635-6779", result);
+        }
+    }
+}
---
+++ SalesAssister/SalesAssister.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SalesAssister.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalesAssister.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a33df4a-09a7-4923-b661-02e3cf81d543")]
