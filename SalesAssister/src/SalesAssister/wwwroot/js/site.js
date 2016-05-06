a513295a5ee7cfe2330061710be650855ce82844
Michael Smith <michaelanthonysmith00@gmail.com>
2016-05-06 08:28:06 -0700

    add ajax to register form

---
+++ SalesAssister/src/SalesAssister/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.register').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '~/../../Account/Register',
+            success: function (result) {
+                $('#registerForm').html(result);
+            }
+        });
+    });
+
+    $('.hide-form1').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '~/../../Home/Index',
+            success: function (result) {
+                $('#homePage').html(result);
+            }
+        });
+    });
+});
