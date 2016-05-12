78c5ca7366a751acdbd7723584dc33a2d87f803d
Michael Smith <michaelanthonysmith00@gmail.com>
2016-05-12 09:25:17 -0700

    move photo once user is logged into linkedin

--- SalesAssister/src/SalesAssister/wwwroot/js/site.js
+++ SalesAssister/src/SalesAssister/wwwroot/js/site.js
@@ -26,3 +26,34 @@
 function myFunction() {
     location.reload();
 }
+
+function OnLinkedInFrameworkLoad() {
+    IN.Event.on(IN, "auth", OnLinkedInAuth);
+}
+
+function OnLinkedInAuth() {
+    IN.API.Profile("me").result(ShowProfileData);
+}
+
+function ShowProfileData(profiles) {
+    var member = profiles.values[0];
+    var id = member.id;
+    var firstName = member.firstName;
+    var lastName = member.lastName;
+    var photo = member.pictureUrl;
+    var headline = member.headline;
+
+    console.log(member);
+    console.log(id);
+    console.log(firstName);
+    console.log(lastName);
+    console.log(headline);
+    //use information captured above
+
+    $(".in").append('<h3>' + headline + '</h3>');
+    $(".photo").append('<img src="' + photo + '"/>');
+}
+
+function logOut() {
+    IN.User.logout(callbackFunction, callbackScope);
+}
